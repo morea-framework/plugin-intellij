2cf6a342d124fab0d30b63e57f3f30ec774cc7ec
bkimcode <bekim3@hawaii.edu>
2023-04-05 15:49:49 -1000

    transfer test
    testing to see if brute force transfer is safe

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# plugin-intellij
-A repository to hold an IntelliJ IDEA plugin to facilitate Morea site development
+# Morea
+
+![Build](https://github.com/Spring2023MoreaFrameworkPlugin/NewPageAction/workflows/Build/badge.svg)
+[![Version](https://img.shields.io/jetbrains/plugin/v/21128-morea.svg)](https://plugins.jetbrains.com/plugin/21128-morea)
+[![Downloads](https://img.shields.io/jetbrains/plugin/d/21128-morea.svg)](https://plugins.jetbrains.com/plugin/21128-morea)
+
+
+<!-- Plugin description -->
+    Plugin to provide Morea developers with additional tools to aid in development.
+     - right-click option on Morea directory to create a new module
+     - right-click option on Morea module directory to create a new morea page
+
+<!-- Plugin description end -->
+
+## Installation
+
+- Using IDE built-in plugin system:
+  
+  <kbd>Settings/Preferences</kbd> > <kbd>Plugins</kbd> > <kbd>Marketplace</kbd> > <kbd>Search for "NewPageAction"</kbd> >
+  <kbd>Install Plugin</kbd>
+  
+- Manually:
+
+  Download the [latest release](https://github.com/Spring2023MoreaFrameworkPlugin/NewPageAction/releases/latest) and install it manually using
+  <kbd>Settings/Preferences</kbd> > <kbd>Plugins</kbd> > <kbd>⚙️</kbd> > <kbd>Install plugin from disk...</kbd>
+
+
+---
+Plugin based on the [IntelliJ Platform Plugin Template][template].
+
+[template]: https://github.com/JetBrains/intellij-platform-plugin-template
---
+++ src/main/java/com/newpageaction/NewAssessmentsAction.java
@@ -0,0 +1,77 @@
+package com.newpageaction;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.ui.InputValidator;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class NewAssessmentsAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        MoreaUtils morea = new MoreaUtils();
+        String input = Messages.showInputDialog("Enter Assessments Page Name:", "New Assessments Page", Messages.getQuestionIcon(), "assessment-example", new InputValidator() {
+            @Override
+            public boolean checkInput(String inputString) {
+                return !inputString.isEmpty();
+            }
+
+            @Override
+            public boolean canClose(String inputString) {
+                return checkInput(inputString);
+            }
+        });
+
+        // Check if name to create is not null
+        if (input != null) {
+            // Check input name matches MOREA naming convention
+            input = morea.toMoreaName(input, "assessment");
+            VirtualFile directory = morea.checkDupes(e, input);
+
+            String finalInput = input;
+            WriteAction.run(() -> {
+                try {
+                    VirtualFile readingFile = directory.createChildData(this, finalInput + ".md");
+                    try (OutputStream outputStream = readingFile.getOutputStream(this)) {
+                        String content = "---\n" +
+                                "title: \"CHANGE ME\"\n" +
+                                "published: true\n" +
+                                "morea_id: " + finalInput + "\n" +
+                                "morea_type: assessment\n" +
+                                "morea_summary: \"CHANGE ME\"\n" +
+                                "morea_sort_order: \n" +
+                                "morea_start_date: \n" +
+                                "morea_labels: \n" +
+                                "---\n\n" +
+                                "## \"CHANGE ME\"\n\n" +
+                                "This is a sample content for the newly creating assessment.md file";
+                        outputStream.write(content.getBytes());
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+        VirtualFile selectedFile = e.getData(PlatformDataKeys.VIRTUAL_FILE);
+        if (selectedFile != null) {
+            VirtualFile parent = selectedFile.getParent();
+            e.getPresentation().setEnabledAndVisible(parent != null && "morea".equals(parent.getName()));
+        } else {
+            e.getPresentation().setEnabledAndVisible(false);
+        }
+    }
+}
