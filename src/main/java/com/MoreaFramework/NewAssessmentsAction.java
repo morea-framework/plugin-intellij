f3fb1181a0588128afd552433b5737aba4ecc15b
Adrian Tam <65895954+adrianwtam@users.noreply.github.com>
2023-04-20 10:26:12 -1000

    not quite done

--- src/main/java/com/MoreaFramework/NewAssessmentsAction.java
+++ src/main/java/com/MoreaFramework/NewAssessmentsAction.java
@@ -7,12 +7,21 @@ import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.ui.InputValidator;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.yaml.snakeyaml.Yaml;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Map;
+import org.yaml.snakeyaml.DumperOptions;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 public class NewAssessmentsAction extends AnAction {
 
+  //Refers to the Folder
+  private VirtualFile selectedFile;
     @Override
     public void actionPerformed(AnActionEvent e) {
         MoreaUtils morea = new MoreaUtils();
@@ -58,6 +67,69 @@ public class NewAssessmentsAction extends AnAction {
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
+
+                VirtualFile moduleFile = selectedFile.findChild("module-" + selectedFile.getName() + ".md");
+                System.out.println("ModuleFile: " + moduleFile);
+                System.out.println("ModuleFile getPath: " + moduleFile.getPath());
+
+                //------Testing-------
+
+              try {
+                // Create a BufferedReader to read the Markdown file
+                BufferedReader reader = new BufferedReader(new FileReader(moduleFile.getPath()));
+                System.out.println("Error 1");
+                // Read the YAML front matter into a string
+                String yamlString = "";
+                String line;
+                int count = 0;
+                while ((line = reader.readLine()) != null) {
+                  if (line.equals("---")) {
+                    if(count == 0){
+                      count++;
+                    }
+                    else {
+                      break;
+                    }
+                  }
+                  yamlString += line + "\n";
+                }
+                System.out.println("Error 2");
+                // Parse the YAML string using SnakeYAML
+                DumperOptions options = new DumperOptions();
+                options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+
+                Yaml yaml = new Yaml(options);
+                Map<String, Object> yamlMap = yaml.load(yamlString);
+
+                // Modify the title and author values
+                System.out.println("yamlString: " + yamlString);
+
+                yamlMap.put("morea_assessments", Arrays.asList(finalInput));
+
+
+                // Serialize the YAML map back into a string
+                String newYamlString = yaml.dump(yamlMap);
+
+                // Print the modified YAML string
+                System.out.println(newYamlString);
+                // Change this to Overwrite the FILE!
+                FileWriter writer = new FileWriter(moduleFile.getPath());
+                writer.write("---\n");
+                writer.write(newYamlString);
+                writer.write("---\n");
+
+                // Close the reader
+                writer.close();
+                reader.close();
+              } catch (IOException e2) {
+                System.out.println("An error occurred while reading the file.");
+                e2.printStackTrace();
+              }
+              System.out.println("Error 5");
+
+
+
+      //------Testing-------
             });
         }
     }
@@ -65,7 +137,7 @@ public class NewAssessmentsAction extends AnAction {
     @Override
     public void update(AnActionEvent e) {
         super.update(e);
-        VirtualFile selectedFile = e.getData(PlatformDataKeys.VIRTUAL_FILE);
+        selectedFile = e.getData(PlatformDataKeys.VIRTUAL_FILE);
         if (selectedFile != null) {
             VirtualFile parent = selectedFile.getParent();
             e.getPresentation().setEnabledAndVisible(parent != null && "morea".equals(parent.getName()));
