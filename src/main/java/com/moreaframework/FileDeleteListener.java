246241fdc80ec97b4f2d9257d185b6e007f71a22
Raphael Anroe Bumanlag <raphael6@hawaii.edu>
2023-04-27 16:26:44 -1000

    Updated FileDeleteListener
    Now handles multiple file deletes in same directory. Does not handle multiple file deletes in multiple directories.

--- src/main/java/com/moreaframework/FileDeleteListener.java
+++ src/main/java/com/moreaframework/FileDeleteListener.java
@@ -19,61 +19,65 @@ public class FileDeleteListener implements AnActionListener {
     public void beforeActionPerformed(@NotNull AnAction action, @NotNull AnActionEvent event) {
 
         if (ActionManager.getInstance().getId(action).equals(IdeActions.ACTION_DELETE)) {
-            // Get deleted file and parent directory
-            VirtualFile deletedFile = event.getData(PlatformDataKeys.VIRTUAL_FILE);
-            VirtualFile parentDir = deletedFile.getParent();
+            // Get deleted file(s) and parent directory
+            VirtualFile[] deletedFiles = event.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
+            VirtualFile parentDir = deletedFiles[0].getParent();
             parentDir.refresh(false, true);
             String moduleName = "module-" + parentDir.getName() + ".md";
 
-            // Get module.md
-            for(VirtualFile file : parentDir.getChildren()) {
-                if (file.getName().equalsIgnoreCase(moduleName)) {
-                    Yaml deletedFileYaml = new Yaml();
-                    Yaml moduleFileYaml = new Yaml();
-                    Yaml newYaml = new Yaml(new CustomRepresenter());
-                    DumperOptions options = new DumperOptions();
+            // iterate through each deleted file
+            for (VirtualFile deletedFile : deletedFiles) {
+                // search for module file
+                for (VirtualFile file : parentDir.getChildren()) {
+                    if (file.getName().equalsIgnoreCase(moduleName)) {
+                        Yaml deletedFileYaml = new Yaml();
+                        Yaml moduleFileYaml = new Yaml();
+                        Yaml newYaml = new Yaml(new CustomRepresenter());
+                        DumperOptions options = new DumperOptions();
 
-                    Document deletedDocument = FileDocumentManager.getInstance().getDocument(deletedFile);
-                    Document moduleDocument = FileDocumentManager.getInstance().getDocument(file);
+                        Document deletedDocument = FileDocumentManager.getInstance().getDocument(deletedFile);
+                        Document moduleDocument = FileDocumentManager.getInstance().getDocument(file);
 
-                    String deletedContent = deletedDocument.getText();
-                    String moduleContent = moduleDocument.getText();
+                        String deletedContent = deletedDocument.getText();
+                        String moduleContent = moduleDocument.getText();
 
-                    int moduleFrontMatterEnd = moduleContent.indexOf("---", 3);
-                    int moduleFrontMatterStart = moduleContent.indexOf("---") + 3;
+                        int moduleFrontMatterEnd = moduleContent.indexOf("---", 3);
+                        int moduleFrontMatterStart = moduleContent.indexOf("---") + 3;
 
-                    int deletedFrontMatterEnd = deletedContent.indexOf("---", 3);
-                    int deletedFrontMatterStart = deletedContent.indexOf("---") + 3;
+                        int deletedFrontMatterEnd = deletedContent.indexOf("---", 3);
+                        int deletedFrontMatterStart = deletedContent.indexOf("---") + 3;
 
-                    String deletedFrontMatter = deletedContent.substring(deletedFrontMatterStart, deletedFrontMatterEnd);
+                        String deletedFrontMatter = deletedContent.substring(deletedFrontMatterStart, deletedFrontMatterEnd);
 
-                    String moduleFrontMatter = moduleContent.substring(moduleFrontMatterStart, moduleFrontMatterEnd);
-                    String moduleRemainingContent = moduleContent.substring(moduleFrontMatterEnd + 3);
+                        String moduleFrontMatter = moduleContent.substring(moduleFrontMatterStart, moduleFrontMatterEnd);
+                        String moduleRemainingContent = moduleContent.substring(moduleFrontMatterEnd + 3);
 
-                    Map<String, Object> deletedFrontMatterData = deletedFileYaml.load(deletedFrontMatter);
-                    Object deletedId = deletedFrontMatterData.get("morea_id");
+                        Map<String, Object> deletedFrontMatterData = deletedFileYaml.load(deletedFrontMatter);
+                        Object deletedId = deletedFrontMatterData.get("morea_id");
 
-                    String deletedID = deletedId.toString();
-                    int typePos = deletedID.indexOf("-");
-                    String type = deletedID.substring(0,typePos);
-                    type = "morea_" + type + "s";
+                        String deletedID = deletedId.toString();
+                        int typePos = deletedID.indexOf("-");
+                        String type = deletedID.substring(0,typePos);
+                        type = "morea_" + type + "s";
 
-                    Map<String, Object> moduleFrontMatterData = moduleFileYaml.load(moduleFrontMatter);
-                    List<Map<String, Object>> moreaType = (List<Map<String, Object>>) moduleFrontMatterData.get(type);
 
-                    moreaType.remove(deletedId);
+                        Map<String, Object> moduleFrontMatterData = moduleFileYaml.load(moduleFrontMatter);
+                        List<Map<String, Object>> moreaType = (List<Map<String, Object>>) moduleFrontMatterData.get(type);
 
-                    options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-                    options.setPrettyFlow(true);
+                        moreaType.remove(deletedId);
 
-                    String updatedYamlFrontMatter = newYaml.dump(moduleFrontMatterData).trim();
-                    String updatedContent = "---\n" + updatedYamlFrontMatter + "\n---" + moduleRemainingContent;
+                        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+                        options.setPrettyFlow(true);
 
-                    ApplicationManager.getApplication().runWriteAction(() -> {
-                        moduleDocument.setText(updatedContent);
-                    });
+                        String updatedYamlFrontMatter = newYaml.dump(moduleFrontMatterData).trim();
+                        String updatedContent = "---\n" + updatedYamlFrontMatter + "\n---" + moduleRemainingContent;
 
-                    break;
+                        ApplicationManager.getApplication().runWriteAction(() -> {
+                            moduleDocument.setText(updatedContent);
+                        });
+
+                        break;
+                    }
                 }
             }
         }
